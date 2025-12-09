ff0c0c3aa7488f6da96d80e2c346072114e38fa0
Jafrin-06 <jafrinjayakumar6@gmail.com>
2025-12-09 15:46:50 +0530

    Create student.py

---
+++ class/student.py
@@ -0,0 +1,18 @@
+#print school name from object
+class student:
+    school_name="ABC"
+    def __init__(self):
+        self.name="Jafrin"
+        self.roll=32
+        print("Name:",self.name," Roll.No:",self.roll)
+objstudent=student()
+print("School Name:",student.school_name)
+
+
+#print school name from object
+class student:
+    school_name="ABC"
+    def __init__(self):
+        self.name="Jafrin"
+        self.roll=32
+print("School Name:",student.school_name)
