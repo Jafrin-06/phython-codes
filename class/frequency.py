e3683398dd243f73154437c733d19bce872df786
Jafrin-06 <jafrinjayakumar6@gmail.com>
2025-12-15 10:28:32 +0530

    Update frequency.py

--- class/frequency.py
+++ class/frequency.py
@@ -1,14 +1,23 @@
-class Frequency:
-    def __init__(self,a):
-        self.a=a
-        self.freq = {}
-    def count_frequency(self):
-        for num in self.a:
-            if num in self.freq:
-                self.freq[num]=self.freq[num]+1
-            else:
-                self.freq[num]=1
-        return self.freq
-a=[1, 2, 1, 3, 2, 1]
-objfreq = Frequency(a)
-print("Frequency:", objfreq.count_frequency())
+# 1. Count frequency of each element (without using collections)
+# Input:
+# [1, 2, 1, 3, 2, 1]
+# Output:
+# {1:3, 2:2, 3:1}
+
+class count:
+    def __init__(self, a):
+        self.a = a
+    def freq(self):
+        visited = []
+        for i in range(len(self.a)):
+            if self.a[i] in visited:
+                continue
+            count = 1
+            for j in range(i + 1, len(self.a)):
+                if self.a[i] == self.a[j]:
+                    count=count+1
+            visited.append(self.a[i])
+            print(self.a[i], ":", count)
+a = [1, 2, 1, 3, 2, 1]
+obj=count(a)
+obj.freq()
